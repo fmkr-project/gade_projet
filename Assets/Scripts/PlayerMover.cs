d1315a790caaafcd3f41c18bc38374c3cf303f6c
thay <91734800+fmkr-project@users.noreply.github.com>
2024-03-18 09:23:26 +0100

    Fix sprint

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -5,8 +5,8 @@ using UnityEngine.AI;
 
 public class PlayerMover : MonoBehaviour
 {
-    [SerializeField] private float walkingSpeed = 20f;
-    [SerializeField] private float sprintSpeed = 45f;
+    [SerializeField] private float walkingSpeed = 3.5f;
+    [SerializeField] private float sprintSpeed = 6f;
 
     private NavMeshAgent _agent;
     
@@ -19,14 +19,13 @@ public class PlayerMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var deltaTime = Time.deltaTime;
         var horizontalMove = Input.GetAxis("Horizontal");
         var verticalMove = Input.GetAxis("Vertical");
 
         var finalSpeed = Input.GetAxis("Sprint") != 0 ? sprintSpeed : walkingSpeed;
+        _agent.speed = finalSpeed;
 
-        var displacement = (Vector3.forward * verticalMove + Vector3.right * horizontalMove)
-                           * (deltaTime * finalSpeed);
+        var displacement = Vector3.forward * verticalMove + Vector3.right * horizontalMove;
         _agent.SetDestination(transform.position + displacement);
     }
 }
