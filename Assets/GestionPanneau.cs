dbb5d27e9134f7bd5fadab0f41e2858b9e651846
Darkgiant24 <43791371+Darkgiant24@users.noreply.github.com>
2024-05-07 14:30:33 -0100

    maj du tutorial

--- Assets/GestionPanneau.cs
+++ Assets/GestionPanneau.cs
@@ -3,12 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class NavigationPanneau : MonoBehaviour
 {
-    public GameObject[] panneaux; // Tableau des panneaux à activer/désactiver
-    private int panneauActifIndex = 0; // Indice du panneau actuellement affiché
+    public GameObject[] panneaux; 
+    private int panneauActifIndex = 0; 
 
     void Start()
     {
-        // Désactive tous les panneaux sauf le premier au démarrage
+       
         for (int i = 1; i < panneaux.Length; i++)
         {
             panneaux[i].SetActive(false);
@@ -17,8 +17,8 @@ public class NavigationPanneau : MonoBehaviour
 
     void Update()
     {
-        // Navigation vers le panneau suivant avec le clic droit de la souris
-        if (Input.GetMouseButtonDown(1)) // 1 correspond au bouton droit de la souris
+        
+        if (Input.GetKeyDown(KeyCode.Return)) // 1 correspond au bouton droit de la souris
         {
             if (panneauActifIndex == panneaux.Length - 1)
             {
@@ -30,8 +30,8 @@ public class NavigationPanneau : MonoBehaviour
             }
         }
 
-        // Navigation vers le panneau précédent avec le clic gauche de la souris
-        if (Input.GetMouseButtonDown(0)) // 0 correspond au bouton gauche de la souris
+        
+        if (Input.GetMouseButtonDown(0)) 
         {
             AfficherPanneauPrecedent();
         }
@@ -40,30 +40,30 @@ public class NavigationPanneau : MonoBehaviour
     // Fonction pour afficher le panneau suivant
     void AfficherPanneauSuivant()
     {
-        // Désactive le panneau actuel
+       
         panneaux[panneauActifIndex].SetActive(false);
 
-        // Passe au panneau suivant (ou reste au dernier panneau s'il est déjà affiché)
+        
         panneauActifIndex = Mathf.Min(panneauActifIndex + 1, panneaux.Length - 1);
 
-        // Active le nouveau panneau
+        
         panneaux[panneauActifIndex].SetActive(true);
     }
 
-    // Fonction pour afficher le panneau précédent
+    
     void AfficherPanneauPrecedent()
     {
-        // Désactive le panneau actuel
+      
         panneaux[panneauActifIndex].SetActive(false);
 
-        // Passe au panneau précédent (ou reste au premier panneau s'il est déjà affiché)
+       
         panneauActifIndex = Mathf.Max(panneauActifIndex - 1, 0);
 
-        // Active le nouveau panneau
+      
         panneaux[panneauActifIndex].SetActive(true);
     }
 
-    // Fonction pour lancer la scène Overworld
+    
     void LancerOverworld()
     {
         SceneManager.LoadScene("Overworld");
